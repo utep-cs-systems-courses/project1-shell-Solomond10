eac609c28d46173f483280f24a0a64935c93af93
student <solomond10@yahoo.com>
2020-09-07 19:52:48 -0600

    Program stores the parentPID and forks the process value. It continues until the processValue is 0 which it then prompt the user to keep going

--- basicShell.py
+++ basicShell.py
@@ -1,6 +1,19 @@
 #! /usr/bin/env python3
 
+import os, sys
+
 while 1:
+
+    parentPid = os.getpid()
+    processValue = os.fork()
+
+    if processValue == 0:
+        print("This is the child")
+        print("Child's PID: ", os.getpid())
+        sys.exit(1)
+    else:
+        print("This the parent")
+        print("Parent's PID: ", parentPid)
 
     print("Enter Y to keep going else the program will stop")
     a = input()
