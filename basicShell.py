6f768f74c30fd8f2094052b0109bbb4f6622023f
student <solomond10@yahoo.com>
2020-09-07 19:33:00 -0600

    program simply prompts the user

---
+++ basicShell.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+
+while 1:
+
+    print("Enter Y to keep going else the program will stop")
+    a = input()
+    a = a.lower()
+    
+    if a == 'y':
+        continue
+    else:
+        break;
