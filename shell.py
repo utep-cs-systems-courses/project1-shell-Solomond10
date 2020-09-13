502450263c0c16ffa1834ecbc004e0186dd2f05c
student <solomond10@yahoo.com>
2020-09-13 02:39:54 -0600

    Child was added but commands are not able to yet be executed

--- shell.py
+++ shell.py
@@ -2,6 +2,7 @@
 
 import os, sys, re
 
+parentPid = os.getpid()
 prompt = "$ "
 
 while True:
@@ -11,7 +12,26 @@ while True:
     if(userCommand == "exit"):
         break;
 
-    #os.fork()
+    processValue = os.fork()
+
+    if processValue < 0:
+        os.write(1,"The fork has failed")
+        sys.exit(1)
+
+    elif processValue == 0:
+        os.write(1,("\nThis is the child").encode())
+        os.write(1,("\nChild's PID: %d" %os.getpid()).encode())
+        time.sleep(1)
+        os.write(1, ("\nChild is being terminated").encode())
+        path = "~/OS/Lab2?project1-shell-Solomond10"
+        os.execve(path,userCommand,os.environ)
+    else:
+        os.write(1,("\nThis the parent").encode())
+        os.write(1,("\nParent's PID: %d" %parentPid).encode())
+
+   # if child > 0:
+    #    print()
+    #else: child == 0:
     
     #os.execve(userCommand)
     
