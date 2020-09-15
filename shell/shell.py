d3e3acca842ea8662b2b1cbb98f371cf7d2eeb4c
student <solomond10@yahoo.com>
2020-09-15 00:48:10 -0600

    The child is able to find the correct path but doesn't exit the program when a wrong command is entered

--- shell/shell.py
+++ shell/shell.py
@@ -2,51 +2,60 @@
 
 import os, sys, re
 
-#parentPid = os.getpid()
-#export PS1
-#= "$ "
+parentPid = os.getpid()
+
+PS1 =  os.environ.get('$PS1')
 
 while True:
 
-       args = ["random", "shell.py"]
-       PS1 =  os.environ.get('PS1')
+       #args = ["random", "shell.py"]
+       
+       #print(PS1)
 
-       if(PS1 == None):
-              PS1 = input("$ ")
-       else:
-             # print("PS1 is not empty")
+       if( PS1 == None):
+              a = input("$ ")
+        #      sys.ps1 = "$ "
 
-              userCommand = input()
+       #a = input("$ ")
               
-              if(userCommand == "exit"):
+       if( PS1 == "exit"):
+              sys.exit(1);
 
-                     sys.exit(1);
-
-              processValue = os.fork()
-
-              if processValue < 0:
-                     os.write(1,"The fork has failed")
-                     sys.exit(1)
-
-              elif processValue == 0:
-                     os.write(1,("\nThis is the child").encode())
-                     os.write(1,("\nChild's PID: %d" %os.getpid()).encode())
-                     os.write(1, ("\nChild is being terminated").encode())
-                                   #print("PS1 is empty")
-                     for dir in re.split(":", os.environ['PATH']):
+       processValue = os.fork()
 
 
-                            prDir = "%s/%s" % (dir, args[0])
+       if processValue < 0:
+              os.write(1,"The fork has failed")
+              sys.exit(1)
 
-                            #print(prDir)
-                            try:
-                                   os.execve(prDir, args ,os.environ)
+       elif processValue == 0: #Child
+              os.write(1,("\nThis is the child").encode())
+              os.write(1,("\nChild's PID: %d" %os.getpid()).encode())
+              os.write(1, ("\nChild is being terminated\n").encode())
+              #print("PS1 is empty")
+              args = [a,"shell.py"]
+              
+              for dir in re.split(":", os.environ['PATH']):
 
-                            except FileNotFoundError:
-                                   pass
+                     prDir = "%s/%s" % (dir, args[0])
 
-              else:
-                     os.write(1,("\nThis the parent").encode())
-                     os.write(1,("\nParent's PID: %d" %parentPid).encode())
-   
+                     try:
+                            os.execve(prDir, args ,os.environ)
+
+                     except FileNotFoundError:
+                            pass
+                     
+              os.write(2, ("Child:    Could not execute %s\n" % args[0]).encode())
+              sys.exit(1)
+
+
+       else: #Parent
+              os.write(1,("\nThis the parent").encode())
+              os.write(1,("\nParent's PID: %d" %parentPid).encode())
+
+       #PS1 = None
+       
+       #sys.exit(1)
+#              else:
+ #                    os.write(1,("Parent: Child %d has been terminated with the exit code %d",os.getPid()))
 #print(" THE END ")
