63afaae4314c48ffae1964c6da51e608d8df178f
student <solomond10@yahoo.com>
2020-09-15 21:52:41 -0600

    Input has been changed

--- shell/shell.py
+++ shell/shell.py
@@ -4,21 +4,15 @@ import os, sys, re
 
 parentPid = os.getpid()
 
-PS1 =  os.environ.get('$PS1')
+PS1 = os.environ.get('PS1')
 
 while True:
 
-       #args = ["random", "shell.py"]
-       
-       #print(PS1)
-
-       if( PS1 == None):
-              a = input("$ ")
-        #      sys.ps1 = "$ "
-
-       #a = input("$ ")
-              
-       if( PS1 == "exit"):
+       if PS1 == None:
+              os.environ['PS1'] = "$ "
+              userCommand = input(os.environ.get('PS1'))
+           
+       if userCommand == "exit":
               sys.exit(1);
 
        processValue = os.fork()
@@ -32,8 +26,8 @@ while True:
               os.write(1,("\nThis is the child").encode())
               os.write(1,("\nChild's PID: %d" %os.getpid()).encode())
               os.write(1, ("\nChild is being terminated\n").encode())
-              #print("PS1 is empty")
-              args = [a,"shell.py"]
+
+              args = [userCommand,"shell.py"]
               
               for dir in re.split(":", os.environ['PATH']):
 
@@ -45,17 +39,11 @@ while True:
                      except FileNotFoundError:
                             pass
                      
-              os.write(2, ("Child:    Could not execute %s\n" % args[0]).encode())
+              os.write(2, ("Child: %d Could not execute \n" %os.getpid()).encode())
+              os.write(1, ("Command: %s was not found\n" % a).encode())
               sys.exit(1)
 
 
        else: #Parent
               os.write(1,("\nThis the parent").encode())
               os.write(1,("\nParent's PID: %d" %parentPid).encode())
-
-       #PS1 = None
-       
-       #sys.exit(1)
-#              else:
- #                    os.write(1,("Parent: Child %d has been terminated with the exit code %d",os.getPid()))
-#print(" THE END ")
