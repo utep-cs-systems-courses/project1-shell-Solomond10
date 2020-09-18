8fac10ba03fb927146717ada80bb75a4c4513d73
student <solomond10@yahoo.com>
2020-09-17 21:49:22 -0600

    PS1 is working correctly

--- shell/shell.py
+++ shell/shell.py
@@ -4,30 +4,60 @@ import os, sys, re
 
 parentPid = os.getpid()
 
-PS1 = os.environ.get('PS1')
+
+
+def getCommand():
+
+       prompt = "$ "
+       
+       if "PS1" in os.environ:
+              prompt = os.environ["PS1"]
+
+       userCommand = input(prompt)
+
+       return userCommand
+
 
 while True:
 
-       if PS1 == None:
-              os.environ['PS1'] = "$ "
-              userCommand = input(os.environ.get('PS1'))
-           
-       if userCommand == "exit":
-              sys.exit(1);
+       userCommand = getCommand()
 
+       if userCommand == "exit":
+              #sys.exit(1);
+              break
+       
        processValue = os.fork()
 
+       cl = userCommand.split() 
+
+       if len(cl) == 2:
+
+              cmd = cl[0]
+              p = cl[1]
+
+              #print (cmd)
+              #print (p)
+       
+              if cmd == "cd":
+                     os.chdir(p)
+
+       print(os.getcwd())
 
        if processValue < 0:
               os.write(1,"The fork has failed")
               sys.exit(1)
 
-       elif processValue == 0: #Child
-              os.write(1,("\nThis is the child").encode())
-              os.write(1,("\nChild's PID: %d" %os.getpid()).encode())
-              os.write(1, ("\nChild is being terminated\n").encode())
+       elif processValue == 0: 
+       #       os.write(1,("\nThis is the child").encode())
+       #       os.write(1,("\nChild's PID: %d" %os.getpid()).encode())
+       #       os.write(1, ("\nChild is being terminated\n").encode())
 
-              args = [userCommand,"shell.py"]
+              #args = [userCommand,"shell.py"]
+              #a2 = "/bin/sh/" #originally
+              a2 = "shell.py"
+              #userCommand = "/bin/"+userCommand 
+              #print(userCommand)
+              args = [userCommand, a2]
               
               for dir in re.split(":", os.environ['PATH']):
 
@@ -39,11 +69,27 @@ while True:
                      except FileNotFoundError:
                             pass
                      
-              os.write(2, ("Child: %d Could not execute \n" %os.getpid()).encode())
-              os.write(1, ("Command: %s was not found\n" % a).encode())
+              os.write(2, ("\nChild: %d Could not execute \n" %os.getpid()).encode())
+              os.write(1, ("Command: %s was not found\n" %userCommand).encode())
               sys.exit(1)
 
 
        else: #Parent
-              os.write(1,("\nThis the parent").encode())
-              os.write(1,("\nParent's PID: %d" %parentPid).encode())
+              os.write(1,("\nThis is the parent. My pid is %d\n"%parentPid).encode())
+              childPid = os.wait()
+              os.write(1, ("From Parent: Child %d was terminated with exit code %d\n" %childPid).encode())
+
+'''def getCommand():
+
+       prompt = "$ "
+       
+       if "PS1" in os.environ:
+              prompt = os.environ["PS1"]
+
+       userCommand = input(prompt)
+
+       return userCommand
+
+#       if userCommand == "exit":
+ #             sys.exit(1);
+'''
